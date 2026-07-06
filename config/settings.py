0a9b8801b9f75beeca5f97bcd2ecd03d7f9d183a
Awab mohammed <awabmoo132@gmail.com>
2026-07-06 05:26:30 -0700

    Configure production database

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os # اتأكد إنك عامل import للـ os في أول الملف فوق خالص
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,16 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pitch_booking',
-        'USER': 'postgres',
-        'PASSWORD': 'awabmo132@',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default="postgresql://postgres:awabmo132@@localhost:5432/pitch_booking",
+        conn_max_age=600,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
